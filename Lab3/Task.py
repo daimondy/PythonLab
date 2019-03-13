18d1cbf5b46bb51efaf475375a4c3f3dbb1190f6
Elchi DIana <dianaelchi@gmail.com>
2019-03-13 21:21:41 +0300

    Done

--- Lab3/Task.py
+++ Lab3/Task.py
@@ -16,7 +16,6 @@ def answer():
 invalid_input = True
 
 
-
 def function():
     print("Для вызова функций введите:")
     typeoffun = int(input("0 - Выход из программы\n1 - Задача №1\n2 - Задача №2\n3 - Задача №3\n4 - Задача №4\n"))
@@ -46,16 +45,48 @@ def function():
             data.append(lines)
         print(data)
 
-
         t = input('Отсортировать по возрастанию (0) или по убыванию (1)?\n')
         t = int(t)
         data.sort(key=lambda line: int(line[3]), reverse=t)
-        for i in range(0,len(data)):
+        for i in range(0, len(data)):
             print(data[i])
         answer()
 
     elif typeoffun == 3:
-        print('Не готово')
+        answer()
+        file = open("product.txt", "r")
+        data = []
+        for lines in file:
+            lines = lines.strip()
+            lines = lines.split(';')
+            data.append(lines)
+
+        number = []
+        max_number = int(data[len(data)-1][0])
+        stop_words = ['STOP', 'stop', 'Stop', 'ALL', 'all', 'All']
+        print('Введите номер товара, у которого хотите увеличить цену товара. Вводите по одному. '
+              'Чтобы остановить ввод номера товара, введите stop или all\n'
+              'Количество товара в списке: ', max_number)
+
+        processing = True
+        while processing:
+            pol_number = input('Номер товара: ')
+            if pol_number.isdigit():
+                pol_number = int(pol_number)
+                if pol_number <= max_number:
+                    number.append(pol_number-1)
+                else:
+                    print('Вы ввели число больше максимального!')
+            elif pol_number in stop_words:
+                processing = False
+            else:
+                print('Вы ввели не число или не стоп-слово!')
+
+        increase = int(input('Введите на сколько вы хотите увеличить цену выбранных вами товаров:\n'))
+        for i in range(0, len(number)):
+            data[number[i]][2] = int(data[number[i]][2]) + increase
+        print(data)
+        answer()
 
     elif typeoffun == 4:
         answer()
@@ -81,7 +112,8 @@ def function():
                 print(text_pol)
         elif boo in otvet_no:
             format = input(
-                "Для каких целей открыте фаила? r - чтение, w - запись(удаление предыдущей записи), a - дозапись, b - открытие в двоичном режиме\n")
+                "Для каких целей открыте фаила? r - чтение, w - запись(удаление предыдущей записи), a - дозапись, "
+                "b - открытие в двоичном режиме\n")
             if format not in ['a', 'w', 'r', 'b']:
                 print('Ошибка! Неверные данные.')
             if format == "r" or "b":
@@ -95,8 +127,12 @@ def function():
         else:
             print("Ошибка! Введены неверные данные.")
         answer()
+
     while invalid_input:
         function()
 
 
 function()
+
+
+print('памагите')
