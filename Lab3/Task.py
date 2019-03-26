646c0fdc8627ec148c6cb29e102b2b34b5f7a67c
Elchi DIana <dianaelchi@gmail.com>
2019-03-26 20:37:42 +0300

    o

--- Lab3/Task.py
+++ Lab3/Task.py
@@ -16,21 +16,12 @@ def answer():
 
 #4 функции
 def katalog():
-    answer()
     print(r"Введите путь к каталогу. Например: C:\Windows\System32")
     dir = input()
     print(len([name for name in os.listdir(dir)
                if os.path.isfile(os.path.join(dir, name))]))
 
 def sortirovka():
-    answer()
-    file = open("product.txt", "r")
-    data = []
-    for lines in file:
-        lines = lines.strip()
-        lines = lines.split(';')
-        data.append(lines)
-    print(data)
     t = input('Отсортировать по возрастанию (0) или по убыванию (1)?\n')
     t = int(t)
     data.sort(key=lambda line: int(line[3]), reverse=t)
@@ -39,7 +30,6 @@ def sortirovka():
     answer()
 
 def increase():
-    answer()
     number = []
     max_number = int(data[len(data) - 1][0])
     stop_words = ['STOP', 'stop', 'Stop', 'ALL', 'all', 'All']
@@ -66,34 +56,37 @@ def increase():
     answer()
 
 def dannie():
-    answer()
     boo = input("Записать новые данные в отдельный фаил?\n")
     if boo in otvet_da:
         name = input('Введите имя фаила:\n') + '.txt'
-        pol_file1 = open(name, "w" , encoding='utf-8')
+        pol_file1 = open(name, "w", encoding='utf-8')
         for i in range(0, len(data)):
-            pol_file1.write("\n")
+            pol_file1.write('\n')
             for j in range(0, len(data)):
-                pol_file1.write(str(data[i][j]))
+                pol_file1.write(str(data[i][j]) + ';')
+        boo = input('Вывести данные?\n')
+        if boo in otvet_da:
+            print(pol_file1)
         file.close()
     elif boo in otvet_no:
-        pol_file2 = open('product.txt', 'w', encoding='utf-8')
+        pol_file2 = open('product.txt', 'r+', encoding='utf-8')
         for i in range(0, len(data)):
-            pol_file2.write("\n")
             for j in range(0, len(data)):
-                pol_file2.write(str(data[i][j]))
+                pol_file2.write(str(data[i][j]) + ';')
         file.close()
+        print('Данные сохранены в фаил product.txt')
     answer()
 
 #начало программы
+file = open("product.txt", "r")
+data = []
+for lines in file:
+    lines = lines.strip()
+    lines = lines.split(';')
+    data.append(lines)
+
 mean = True
 while mean:
-    file = open("product.txt", "r")
-    data = []
-    for lines in file:
-        lines = lines.strip()
-        lines = lines.split(';')
-        data.append(lines)
     #меню
     print("Для вызова функций введите: ")
     typeoffun = int(input("0 - Выход из программы\n1 - Задача №1\n2 - Задача №2\n3 - Задача №3\n4 - Задача №4\n"))
